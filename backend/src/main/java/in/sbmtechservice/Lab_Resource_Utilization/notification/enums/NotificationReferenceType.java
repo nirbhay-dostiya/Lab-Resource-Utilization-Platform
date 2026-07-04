1dbd42afdc20d2eb683f8f4ac07a7394b06bc46d
nirbhay-dostiya <nirbhaykumarraj1998@gmail.com>
2026-07-05 02:18:22 +0530

    module 7:
    Notification & Alert Module with entity and enums

---
+++ backend/src/main/java/in/sbmtechservice/Lab_Resource_Utilization/notification/enums/NotificationReferenceType.java
@@ -0,0 +1,9 @@
+package in.sbmtechservice.Lab_Resource_Utilization.notification.enums;
+
+public enum NotificationReferenceType {
+    BOOKING,
+    WAITLIST,
+    MAINTENANCE,
+    SHARING_REQUEST
+}
+
---
+++ backend/src/main/java/in/sbmtechservice/Lab_Resource_Utilization/notification/enums/NotificationStatus.java
@@ -0,0 +1,7 @@
+package in.sbmtechservice.Lab_Resource_Utilization.notification.enums;
+
+public enum NotificationStatus {
+    PENDING,
+    SENT,
+    FAILED
+}
