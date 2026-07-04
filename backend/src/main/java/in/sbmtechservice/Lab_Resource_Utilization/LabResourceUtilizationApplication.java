49ecf1b7280c6c437b4ecde4a6a1682cf8dd4236
nirbhay-dostiya <nirbhaykumarraj1998@gmail.com>
2026-07-04 23:01:27 +0530

    Initial commit with backend and frontend

---
+++ backend/src/main/java/in/sbmtechservice/Lab_Resource_Utilization/LabResourceUtilizationApplication.java
@@ -0,0 +1,13 @@
+package in.sbmtechservice.Lab_Resource_Utilization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabResourceUtilizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabResourceUtilizationApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/in/sbmtechservice/Lab_Resource_Utilization/LabResourceUtilizationApplicationTests.java
@@ -0,0 +1,13 @@
+package in.sbmtechservice.Lab_Resource_Utilization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabResourceUtilizationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
