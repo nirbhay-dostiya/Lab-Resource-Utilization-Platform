f56eada8cb599180121a51b3cc482d96e248e072
nirbhay-dostiya <nirbhaykumarraj1998@gmail.com>
2026-07-08 22:06:55 +0530

    Repository for module : 6
    Maintenance & Calibration Tracking

--- backend/src/main/java/in/sbmtechservice/Lab_Resource_Utilization/maintenance/enums/CalibrationStatus.java
+++
@@ -1,7 +0,0 @@
-package in.sbmtechservice.Lab_Resource_Utilization.maintenance.enums;
-
-public enum CalibrationStatus {
-    VALID,
-    EXPIRED,
-    FAILED
-}
--- backend/src/main/java/in/sbmtechservice/Lab_Resource_Utilization/maintenance_calibration/repository/MaintenanceRepository.java
+++
@@ -1,4 +0,0 @@
-package in.sbmtechservice.Lab_Resource_Utilization.maintenance_calibration.repository;
-
-public class MaintenanceRepository {
-}
