99dd0875d56ede75f81eaa2528d556f7f649d11c
nirbhay-dostiya <nirbhaykumarraj1998@gmail.com>
2026-07-08 23:25:57 +0530

    create dto for user auth.

---
+++ backend/src/main/java/in/sbmtechservice/Lab_Resource_Utilization/auth_user/dto/AuthenticationRequest.java
@@ -0,0 +1,15 @@
+package in.sbmtechservice.Lab_Resource_Utilization.auth_user.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthenticationRequest {
+    private String email;
+    private String password;
+}
